8720b4643e8a93afb7403a0e7339dbef21585a9f
Juan Leon <48931308+Leongon@users.noreply.github.com>
2021-10-19 13:16:54 -0500

    19/10/21

---
+++ static/src/js/recorre.js
@@ -0,0 +1,19 @@
+recorrer_embed=[
+    {
+        id:1
+
+    },
+    {
+        id:2
+
+    },
+    {
+        id:3
+
+    }
+];
+
+for (a in recorrer_embed){
+    console.log(recorrer_embed[0])
+}
+
