84d26cad100dfa93135a072785070278bd8c4a8e
chipermas <80859279+chipermas@users.noreply.github.com>
2021-10-27 10:50:08 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class DevelopmentConfig():
     MYSQL_DATABASE_HOST='localhost'
     MYSQL_DATABASE_USER='root'
     MYSQL_DATABASE_PASSWORD=''
-    MYSQL_DATABASE_DB='dbindia'
+    MYSQL_DATABASE_DB='dbdesire'
 
 config ={
     'development':DevelopmentConfig
