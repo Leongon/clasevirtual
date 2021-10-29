b6a60ad3a77fc594fa6bfb9cff9e191b60522c3a
Juan Leon <48931308+Leongon@users.noreply.github.com>
2021-10-28 23:29:09 -0500

    28/10/21

--- static/src/js/datos/login.js
+++ static/src/js/datos/login.js
@@ -1,5 +1,5 @@
-var form = document.getElementById("form");
-var alertdanger = document.getElementsByClassName("alert")
+var formlogin = document.getElementById("form");
+
 
 /*
 form.addEventListener("submit",function(e){
@@ -24,9 +24,8 @@ form.addEventListener("submit",function(e){
 })
 */
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    var datos = new FormData(form);
+function login () {
+    var datos = new FormData(formlogin);
     var datojson = { "usuario": datos.get("email"), "pass": datos.get("pass") }
     console.log(datojson)
     axios({
@@ -37,10 +36,129 @@ form.addEventListener('submit', function (e) {
         .then(res => {
             console.log(res.data)
             if (res.data === "Bienvenido") {
-                $(".alert").html('<div class="flex items-center bg-green-50 text-center"> <svg xmlns="http://www.w3.org/2000/svg"     class="w-16 h-16 rounded-2xl p-3 border border-blue-100 text-blue-400 bg-blue-50" fill="none"     viewBox="0 0 24 24" stroke="currentColor">      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path> </svg>  <div class="flex flex-col ml-3"> <div class="font-medium leading-none">Bienvenidos</div>  </div>  </div>')  
+                $(".alert").html('<div class="flex items-center bg-green-50 text-center"> <svg xmlns="http://www.w3.org/2000/svg"     class="w-16 h-16 rounded-2xl p-3 border border-blue-100 text-blue-400 bg-blue-50" fill="none"     viewBox="0 0 24 24" stroke="currentColor">      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path> </svg>  <div class="flex flex-col ml-3"> <div class="font-medium leading-none">'+res.data+'</div>  </div>  </div>')  
             } else {
-                $(".alert").html('<div class="flex items-center bg-red-400 text-center"> <svg xmlns="http://www.w3.org/2000/svg"     class="w-16 h-16 rounded-2xl p-3 border border-blue-100 text-blue-400 bg-blue-50" fill="none"     viewBox="0 0 24 24" stroke="currentColor">      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path> </svg>  <div class="flex flex-col ml-3"> <div class="font-medium leading-none">Contraseña o usuario incorrecto</div>  </div>  </div>') 
+                $(".alert").html('<div class="flex items-center bg-red-400 text-center"> <svg xmlns="http://www.w3.org/2000/svg"     class="w-16 h-16 rounded-2xl p-3 border border-blue-100 text-blue-400 bg-blue-50" fill="none"     viewBox="0 0 24 24" stroke="currentColor">      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path> </svg>  <div class="flex flex-col ml-3"> <div class="font-medium leading-none">'+res.data+'</div>  </div>  </div>') 
             }
         })
+}
 
-})
+$.validator.setDefaults( {
+    submitHandler: function () {
+       login()
+    }
+ });
+ $(document).ready(function(){
+    $('#form').validate({
+       rules: {
+          email: {
+             required: true
+          },
+          pass: {
+             required: true,
+                
+          },
+        
+          agree: "required"
+       },
+       messages: {
+          email: {
+             required: "ingrese su usuario",
+          },
+          
+          pass: {
+             required: "Por favor ingresa una contraseña",
+             minlength: "Tu contraseña debe ser de no menos de 5 caracteres de longitud"
+          },
+          
+       },
+       errorElement: "em",
+       errorPlacement: function (error, element) {
+          // Add the `help-block` class to the error element
+          error.addClass("help-block");
+ 
+          if (element.prop( "type" ) === "checkbox") {
+             error.insertAfter(element.parent("label") );
+          } else {
+             error.insertAfter(element);
+          }
+       }
+    });
+ });
+
+ $(document).ready(function(){
+    $('#form_registro').validate({
+       rules: {
+           
+        Nombre: {
+            required: true,
+            
+
+         },
+         Apellido: {
+            required: true
+         }, 
+         tel: {
+            required: true,
+            number: true,
+            rangelength:[9,9]
+         }, 
+          email: {
+             required: true
+          },
+          pass: {
+             required: true,
+                
+          },
+          pass_confirmar: {
+            required: true,
+            minlength: 5,
+            equalTo: "#pass"
+         },
+          agree: "required"
+       },
+       messages: {
+        Nombre: {
+            required: "Por favor llene sus Nombres",
+         },
+         Apellido: {
+            required: "Por favor llene sus Apellidos",
+         },
+         tel:{
+            required:"ingrese su numero celular",
+            number: "Solo acepta numeros ",
+            rangelength:" ingrese su nuemero celular de 9 digitos"
+          },      
+         
+          email: {
+             required: "ingrese su usuario",
+          },
+          
+          pass: {
+             required: "Por favor ingresa una contraseña",
+             minlength: "Tu contraseña debe ser de no menos de 5 caracteres de longitud"
+          },
+          pass_confirmar: {
+            required: "Ingresa un password",
+            minlength: "Tu contraseña debe ser de no menos de 5 caracteres de longitud",
+            equalTo: "Por favor ingresa la misma contraseña de arriba"
+         },
+
+          
+       },
+       errorElement: "em",
+       errorPlacement: function (error, element) {
+          // Add the `help-block` class to the error element
+          error.addClass("help-block");
+ 
+          if (element.prop( "type" ) === "checkbox") {
+             error.insertAfter(element.parent("label") );
+          } else {
+             error.insertAfter(element);
+          }
+       }
+    });
+ });
+
+ 
+
