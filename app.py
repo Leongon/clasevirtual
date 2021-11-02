15545bf63a3d9b4b111465806d466f4121a1b09b
Juan Leon <48931308+Leongon@users.noreply.github.com>
2021-11-02 00:38:07 -0500

    2/11/21

--- app.py
+++ app.py
@@ -20,19 +20,19 @@ def inventario():
     return render_template("login.html")
 def paginanoencontrada(e):
     return "<h1>Error 404</h1><h2>La pagina que usted desea visualizar no existe o es incorrecta</h2>"
-
 @app.route('/registrar')
 def registrar():
     return render_template("registrar.html")
-
 @app.route('/panel')
 def panel():
     return render_template("panel.html")
-
+@app.route('/curso')
+def curso():
+    return render_template("Curso.html")
+        
 @app.route('/inicio')
 def inicio():
     return render_template("inicio.html")    
-
 # Controladores
 @app.route('/apiLogin', methods=['POST'])
 def apiLogin():
