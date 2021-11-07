0b6678dd83993d3fbcea665219400458b1a18544
Juan Leon <48931308+Leongon@users.noreply.github.com>
2021-11-07 18:21:21 -0500

    7/11/21

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import error
 from flask import Flask, render_template, request, make_response, jsonify, redirect, session, escape, url_for
 from flask.json import JSONEncoder
 from flaskext.mysql import MySQL
@@ -30,14 +31,13 @@ def registrar():
     if "usuario" in session:
         return render_template("cursos.html")
     return render_template("registrar.html")
-@app.route('/panel')
+@app.route('/perfil')
 def panel():
-    if "usuario" in session:
-        return render_template("panel.html")
-    return render_template("registrar.html")
+    return render_template("perfil.html")
 @app.route('/cursos')
 def cursos():
     try:
+        
         sql="SELECT usuarios.usuario, usuarios.nombres, usuarios.apellidos, usuarios.correo, usuarios.telefono, cursos.curso, cursos.descripcion, cursos.precio, rol.rol, nivel.nivel, cursos.idcursos FROM (rol INNER JOIN usuarios ON rol.idrol = usuarios.fkrol) INNER JOIN (nivel INNER JOIN cursos ON nivel.idnivel = cursos.fknivel) ON usuarios.id = cursos.fkprofesor;"
         conn = conexion.connect()
         cursor = conn.cursor()
@@ -47,13 +47,25 @@ def cursos():
         for fila in datos:
             producto={'usuario':fila[0],'nombres':fila[1],'apellidos':fila[2],'correo':fila[3],'telefono':fila[4],'curso':fila[5],'descripcion':fila[6],'precio':fila[7],'rol':fila[8],'nivel':fila[9], 'idcurso' :fila[10]}
             cursos.append(producto)
-        conn.commit()        
-        return render_template("Cursos.html",curso=cursos)
+        conn.commit()
+        if "usuario" in session:
+            sql1="SELECT a.id, a.usuario,c.curso,d.nivel,e.nombres  FROM usuarios a INNER JOIN cursoalumno b on a.id=b.fkusuario INNER JOIN cursos c on b.fkcurso = c.idcursos INNER JOIN nivel d on c.fknivel=d.idnivel INNER JOIN usuarios e on c.fkprofesor=e.id WHERE a.usuario='"+ session["usuario"] +"' "
+            conn = conexion.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql1)
+            datos=cursor.fetchall()
+            cursoalumno=[]
+            for fila in datos:
+                producto={'id':fila[0],'usuario':fila[1],'curso':fila[2],'nivel':fila[3],'nombres':fila[4]}
+                cursoalumno.append(producto)
+            conn.commit()
+            return render_template("Cursos.html",curso=cursos,cursoalumnos=cursoalumno)
+        else:
+            return render_template("Cursos.html",curso=cursos)        
     except:
         conn.commit()
         return jsonify({'mensaje':"Error en la base de datos"})
-    
-
+        
 @app.route('/curso')    
 def curso():    
     curse = request.args.get('curso', 'No tienes permiso para acceder')
@@ -74,6 +86,7 @@ def apiLogin():
         datos=cursor.fetchall()        
         if not len(datos) == 0:
             session["usuario"]= datos[0][1]
+            session["nombre"]= datos[0][3]
             return jsonify("Bienvenido")
         else:
             return jsonify("No existe el usuario o la contraseña es incorrecta")
