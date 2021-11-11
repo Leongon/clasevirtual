394805b40a3e9f6011f7e045b9bb90f5141b4c09
chipermas <80859279+chipermas@users.noreply.github.com>
2021-11-10 19:03:52 -0500

    Add files via upload

---
+++ Dump20211110/dbdesire_cursoalumno.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbdesire
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.14-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursoalumno`
+--
+
+DROP TABLE IF EXISTS `cursoalumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursoalumno` (
+  `idcursoalumno` int(11) NOT NULL AUTO_INCREMENT,
+  `fkusuario` int(11) NOT NULL,
+  `fkcurso` int(11) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcursoalumno`),
+  KEY `fk_cursoProfesor_usuarios1_idx` (`fkusuario`),
+  KEY `fk_cursoProfesor_cursos1_idx` (`fkcurso`),
+  CONSTRAINT `fk_cursoProfesor_cursos1` FOREIGN KEY (`fkcurso`) REFERENCES `cursos` (`idcursos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cursoProfesor_usuarios1` FOREIGN KEY (`fkusuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursoalumno`
+--
+
+LOCK TABLES `cursoalumno` WRITE;
+/*!40000 ALTER TABLE `cursoalumno` DISABLE KEYS */;
+INSERT INTO `cursoalumno` VALUES (1,1,1,'1'),(2,1,2,'1'),(3,27,1,'1'),(4,57,2,'1');
+/*!40000 ALTER TABLE `cursoalumno` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-10 19:03:03
